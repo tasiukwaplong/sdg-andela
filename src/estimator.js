208bceba81ad90c46b2afd6713402baa3c1ea017
Tasiu Kwaplong <tasiukwaplong@gmail.com>
2020-04-12 07:43:22 -0700

    Completed challenge two

--- src/estimator.js
+++ src/estimator.js
@@ -1,13 +1,19 @@
-const roundNumber = (numb) => Math.floor(numb);
+const roundNumber = (numb) => {
+  const stringNum = numb.toString().split('.')[0];
+  return Number(stringNum);
+};
 
 const covid19ImpactEstimator = (data) => {
-  const { timeToElapse, reportedCases } = data;
+  const { timeToElapse, reportedCases, totalHospitalBeds } = data;
   // impact estimation
   const currentlyInfected = (multiples) => reportedCases * multiples;
   const infectionsByRequestedTime = (multiples) => {
     return currentlyInfected(multiples) * 2 ** roundNumber(timeToElapse / 3);
   };
   const severeCasesByRequestedTime = (multiples) => 0.15 * infectionsByRequestedTime(multiples);
+  const hospitalBedsByRequestedTime = (multiples) => {
+    return roundNumber(totalHospitalBeds * 0.35 - severeCasesByRequestedTime(multiples));
+  };
   // output
   return {
     data,
@@ -15,12 +21,14 @@ const covid19ImpactEstimator = (data) => {
       impact: {
         currentlyInfected: currentlyInfected(10),
         infectionsByRequestedTime: infectionsByRequestedTime(10),
-        severeCasesByRequestedTime: severeCasesByRequestedTime(10)
+        severeCasesByRequestedTime: severeCasesByRequestedTime(10),
+        hospitalBedsByRequestedTime: hospitalBedsByRequestedTime(10)
       },
       severeImpact: {
         currentlyInfected: currentlyInfected(50),
         infectionsByRequestedTime: infectionsByRequestedTime(50),
-        severeCasesByRequestedTime: severeCasesByRequestedTime(50)
+        severeCasesByRequestedTime: severeCasesByRequestedTime(50),
+        hospitalBedsByRequestedTime: hospitalBedsByRequestedTime(50)
       }
     }
   };
