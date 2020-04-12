13adc6e60b2c2f09211b83416f68f0b085c903bb
Tasiu Kwaplong <tasiukwaplong@gmail.com>
2020-04-12 09:59:58 -0700

    Completed challenge 3 with wrong computation for dolarsinflight

--- src/estimator.js
+++ src/estimator.js
@@ -3,16 +3,39 @@ const roundNumber = (numb) => {
   return Number(stringNum);
 };
 
+const nomaliseToDays = (periodType, timeToElapse) => {
+  switch (periodType) {
+    case 'days':
+      return timeToElapse;
+    case 'weeks':
+      return timeToElapse * 7;
+    case 'months':
+      return timeToElapse * 30;
+    default:
+      return null;
+  }
+};
+
 const covid19ImpactEstimator = (data) => {
-  const { timeToElapse, reportedCases, totalHospitalBeds } = data;
+  const {
+    timeToElapse, reportedCases, totalHospitalBeds, region, periodType
+  } = data;
   // impact estimation
-  const currentlyInfected = (multiples) => reportedCases * multiples;
-  const infectionsByRequestedTime = (multiples) => {
-    return currentlyInfected(multiples) * 2 ** roundNumber(timeToElapse / 3);
+  const currentlyInfected = (i) => reportedCases * i;
+  const infectionsByRequestedTime = (i) => {
+    return currentlyInfected(i) * 2 ** roundNumber(timeToElapse / 3);
   };
-  const severeCasesByRequestedTime = (multiples) => 0.15 * infectionsByRequestedTime(multiples);
-  const hospitalBedsByRequestedTime = (multiples) => {
-    return roundNumber(totalHospitalBeds * 0.35 - severeCasesByRequestedTime(multiples));
+  const severeCasesByRequestedTime = (i) => 0.15 * infectionsByRequestedTime(i);
+  const hospitalBedsByRequestedTime = (i) => {
+    return roundNumber(totalHospitalBeds * 0.35 - severeCasesByRequestedTime(i));
+  };
+  const casesForICUByRequestedTime = (i) => 0.05 * infectionsByRequestedTime(i);
+  const casesForVentilatorsByRequestedTime = (i) => {
+    return roundNumber(0.02 * infectionsByRequestedTime(i));
+  };
+  const dollarsInFlight = (i) => {
+    return roundNumber((infectionsByRequestedTime(i) * region.avgDailyIncomeInUSD
+      * region.avgDailyIncomePopulation) / nomaliseToDays(periodType, timeToElapse));
   };
   // output
   return {
@@ -22,13 +45,19 @@ const covid19ImpactEstimator = (data) => {
         currentlyInfected: currentlyInfected(10),
         infectionsByRequestedTime: infectionsByRequestedTime(10),
         severeCasesByRequestedTime: severeCasesByRequestedTime(10),
-        hospitalBedsByRequestedTime: hospitalBedsByRequestedTime(10)
+        hospitalBedsByRequestedTime: hospitalBedsByRequestedTime(10),
+        casesForICUByRequestedTime: casesForICUByRequestedTime(10),
+        casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime(10),
+        dollarsInFlight: dollarsInFlight(10)
       },
       severeImpact: {
         currentlyInfected: currentlyInfected(50),
         infectionsByRequestedTime: infectionsByRequestedTime(50),
         severeCasesByRequestedTime: severeCasesByRequestedTime(50),
-        hospitalBedsByRequestedTime: hospitalBedsByRequestedTime(50)
+        hospitalBedsByRequestedTime: hospitalBedsByRequestedTime(50),
+        casesForICUByRequestedTime: casesForICUByRequestedTime(50),
+        casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime(50),
+        dollarsInFlight: dollarsInFlight(50)
       }
     }
   };
@@ -50,5 +79,4 @@ const covid19ImpactEstimator = (data) => {
 // };
 
 // console.log(covid19ImpactEstimator(dummyData));
-
 export default covid19ImpactEstimator;
